eb928885df47b6ad7d6406a24a3c043343194bba
Blopez1098t <113131780+Blopez1098t@users.noreply.github.com>
2023-12-15 23:52:35 +0000

    video 8

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode);
-console.log('the parent element is:', bookList.parentElement.parentElement);
+console.log("bookList next sibling is: ", bookList.nextSibling);
+console.log("bookList next element sibling is: ", bookList.nextElementSibling);
 
-console.log(bookList.children);
+console.log("bookList previous sibling is: ", bookList.previousSibling);
+console.log("bookList previous element sibling is: ", bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
